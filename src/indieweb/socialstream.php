a40837691ee3aec2865784897709c4768ef383a4
BenRoberts <ben@cs.uri.edu>
2015-10-21 16:57:15 -0400

    cleaner parsing

--- src/indieweb/socialstream.php
+++ src/indieweb/socialstream.php
@@ -1,24 +1,55 @@
 <?php
 namespace IndieWeb\socialstream;
 
-function single_item_array_reduce($in)
+function clean_node($in)
 {
-    if(is_array($in)){
-        if(count($in) == 1){
-            return single_item_array_reduce($in[0]);
-        } else {
-            foreach( $in as &$item){
-                $item = single_item_array_reduce($item);
-            }
-            return $in;
-        }
-    } else {
-        return $in;
+    //single item handling
+    if (!is_array($in)) {
+        return clean_item($in);
     }
+
+    //array handling
+    $in = clean_array($in);
+
+    //$res = array();
+    foreach ($in as &$item) {
+        $item = clean_node($item);
+    }
+
+
+    if (count($in) == 1) {
+        return array_shift($in);
+        // no need to recurse here as this item
+        // has to have already been cleaned by recursive call above
+    }
+
+    if (empty($in)) {
+        return null;
+    }
+
+    return $in;
+
+}
+
+function clean_array($in)
+{
+    if (isset($in['rels'])) {
+        unset($in['rels']);
+    }
+    return $in;
+}
+
+function clean_item($in)
+{
+    if (is_string($in)) {
+        $in = preg_replace('/\s+/', ' ', $in);
+    }
+
+    return $in;
 }
 
 function convert($mf)
 {
-   return json_encode(single_item_array_reduce($mf),JSON_PRETTY_PRINT);
+    return json_encode(clean_node($mf), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 }
 
