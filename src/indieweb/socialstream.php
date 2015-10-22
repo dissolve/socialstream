8bbd0efa64fb8363f45d8e5554d05f6dca5628f8
BenRoberts <ben@cs.uri.edu>
2015-10-22 13:34:48 -0400

    playing with more possibilities for converting

--- src/indieweb/socialstream.php
+++ src/indieweb/socialstream.php
@@ -40,19 +40,35 @@ function clean_array_before_recurse($in)
     if (isset($in['rels'])) {
         unset($in['rels']);
     }
-    if (isset($in['type'])) {
-        unset($in['type']);
-    }
     if (isset($in['value'])) {
         unset($in['value']);
     }
+    if (isset($in['properties'])) {
+        $prop = $in['properties'];
+        unset($in['properties']);
+        $in = array_merge($in , $prop);
+        
+    }
+    if(isset($in['url']) && is_array($in['url'])){
+        $in['url'] = array_unique($in['url']);
+    }
+    if(isset($in['name']) && is_array($in['name'])){
+        $in['name'] = array_unique($in['name']);
+    }
+    
     return $in;
 }
 
 function clean_array_after_recurse($in)
 {
     if (isset($in['url']) && !is_array($in['url']) && !isset($in['@id'])) {
-       $in['@id'] = $in['url']; 
+        $in['@id'] = $in['url']; 
+    }
+    if (isset($in['type']) && !is_array($in['type'])) {
+        $new_val = preg_replace('/^h-/', '', $in['type']);
+        //do more logic here
+        $in['@type'] = $new_val;
+        unset($in['type']);
     }
     return $in;
 }
@@ -66,8 +82,14 @@ function clean_item($in)
     return $in;
 }
 
-function convert($mf)
+function convert($mf, $lang = 'en')
 {
-    return json_encode(clean_node($mf), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    $cleaned = clean_node($mf);
+    if(!isset($cleaned['type']) || !isset($cleaned['type'])){
+        $cleaned['children'] = $cleaned;
+    }
+    $cleaned['@lang'] = $lang;
+
+    return json_encode($cleaned, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 }
 
