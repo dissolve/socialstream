e35ca0f0ed8df6f7a842c02224c1e6539a4c57ac
Ben Roberts <ben@directdigitalllc.com>
2016-03-22 14:26:16 -0400

    don't do first level over-optimization

--- src/indieweb/mf2stream.php
+++ src/indieweb/mf2stream.php
@@ -1,7 +1,20 @@
 <?php
 namespace IndieWeb\mf2stream;
 
-function reference_format($obj){
+function reference_format($obj)
+{
+    $references = array();
+    foreach($obj['items'] as $key => $val){
+        $obj['items'][$key] = reference_format_recurse($val);
+        $references = array_merge($references,$obj['items'][$key]['references']);
+        unset($obj['items'][$key]['references']);
+    }
+    $obj['references'] = $references;
+    return $obj;
+}
+
+function reference_format_recurse($obj)
+{
     if(!is_array($obj)){
         return $obj;
     }
@@ -13,7 +26,7 @@ function reference_format($obj){
             isset($val['type']) &&
             !is_array($val['properties']['url'][0])){
 
-            $parsed_obj = reference_format($val);
+            $parsed_obj = reference_format_recurse($val);
             if(isset($parsed_obj['references'])){
                 $references = array_merge($references,$parsed_obj['references']);
                 unset($parsed_obj['references']);
@@ -22,7 +35,7 @@ function reference_format($obj){
             $references[$val['properties']['url'][0]] = $parsed_obj;
             $obj[$key] = $val['properties']['url'][0];
         } else {
-            $obj[$key] = reference_format($val);
+            $obj[$key] = reference_format_recurse($val);
             if(isset($obj[$key]['references'])){
                 $references = array_merge($references,$obj[$key]['references']);
                 unset($obj[$key]['references']);
